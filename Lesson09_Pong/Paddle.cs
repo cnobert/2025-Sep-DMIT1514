a0b09ff0ba02074c18cf43215435f366248b8ee2
cnobert <cnobert@nait.ca>
2025-10-20 13:57:57 -0600

    Paddle class

--- Lesson09_Pong/Paddle.cs
+++ Lesson09_Pong/Paddle.cs
@@ -3,19 +3,25 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Lesson09_Pong;
 
-public class Ball
+public class Paddle
 {
     private Rectangle _gameBoundingBox;
+
     private Vector2 _dimensions, _position, _direction;
     private float _speed;
-    
+
     private Texture2D _pixel;
-    internal void Initialize(Vector2 position, float speed, Vector2 direction, Vector2 dimensions, Rectangle gameBoundingBox)
+
+    internal Vector2 Direction
+    {
+        set => _direction = value;
+    }
+
+    internal void Initialize(Vector2 position, float speed, Vector2 dimensions, Rectangle gameBoundingBox)
     {
         _gameBoundingBox = gameBoundingBox;
         _position = position;
         _speed = speed;
-        _direction = direction;
         _dimensions = dimensions;
     }
     internal void LoadContent(GraphicsDevice graphicsDevice)
@@ -24,28 +30,23 @@ public class Ball
         _pixel = new Texture2D(graphicsDevice, 1, 1);
         _pixel.SetData(new[] { Color.White });
     }
+
     internal void Update(GameTime gameTime)
     {
         _position += _direction * _speed * (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-        // bounce ball off left and right sides
-        if (_position.X <= _gameBoundingBox.Left || (_position.X + _dimensions.X) >= _gameBoundingBox.Right)
+        if (_position.Y <= _gameBoundingBox.Top)
         {
-            _direction.X *= -1;
+            _position.Y = _gameBoundingBox.Top;
         }
-        // bounce ball off top and bottom
-        if
-        (
-            _position.Y <= (_gameBoundingBox.Top)
-            || (_position.Y + _dimensions.Y) >= (_gameBoundingBox.Bottom)
-        )
+        else if ((_position.Y + _dimensions.Y) >= _gameBoundingBox.Bottom)
         {
-            _direction.Y *= -1;
+            _position.Y = _gameBoundingBox.Bottom - _dimensions.Y;
         }
     }
+
     internal void Draw(SpriteBatch spriteBatch)
     {
-        // draw ball as a stretched 1x1 pixel
         Rectangle ballRect = new Rectangle(
             (int)_position.X,
             (int)_position.Y,
@@ -54,4 +55,5 @@ public class Ball
         );
         spriteBatch.Draw(_pixel, ballRect, Color.White);
     }
+
 }
--- Lesson09_Pong/Pong.cs
+++ Lesson09_Pong/Pong.cs
@@ -21,11 +21,7 @@ public class Pong : Game
 
     private Ball[] _balls;
 
-    private Vector2 _paddleDimensions, _paddlePosition, _paddleDirection;
-    private float _paddleSpeed;
-
-    // runtime 1x1 pixel for drawing primitives (no Content texture required)
-    private Texture2D _pixel;
+    private Paddle _paddle;
 
     public Pong()
     {
@@ -43,20 +39,24 @@ public class Pong : Game
         _playAreaBoundingBox = new Rectangle(0, 0, _WindowWidth, _WindowHeight);
 
         _balls = new Ball[4];
-        for(int c = 0; c < 4; c++)
+        for (int c = 0; c < 4; c++)
         {
             _balls[c] = new Ball();
 
-            Vector2 ballPosition = new Vector2(c+50 * _Scale, c+65 * _Scale);
+            Vector2 ballPosition = new Vector2(c + 50 * _Scale, c + 65 * _Scale);
             float ballSpeed = 100 * _Scale;
             Vector2 ballDirection = new Vector2(-1, -1);
             Vector2 ballDimensions = new Vector2(_BallWidthAndHeight, _BallWidthAndHeight);
             _balls[c].Initialize(ballPosition, ballSpeed, ballDirection, ballDimensions, _playAreaBoundingBox);
         }
 
-        _paddlePosition = new Vector2(210 * _Scale, 75 * _Scale);
-        _paddleSpeed = 100 * _Scale;
-        _paddleDimensions = new Vector2(_PaddleWidth, _PaddleHeight);
+        _paddle = new Paddle();
+
+        Vector2 paddlePosition = new Vector2(210 * _Scale, 75 * _Scale);
+        float paddleSpeed = 100 * _Scale;
+        Vector2 paddleDimensions = new Vector2(_PaddleWidth, _PaddleHeight);
+
+        _paddle.Initialize(paddlePosition, paddleSpeed, paddleDimensions, _playAreaBoundingBox);
 
         base.Initialize();
     }
@@ -65,14 +65,11 @@ public class Pong : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // create a 1x1 white pixel we can stretch to draw rectangles
-        _pixel = new Texture2D(GraphicsDevice, 1, 1);
-        _pixel.SetData(new[] { Color.White });
-
         foreach (Ball ball in _balls)
         {
             ball.LoadContent(GraphicsDevice);
         }
+        _paddle.LoadContent(GraphicsDevice);
 
     }
 
@@ -83,33 +80,22 @@ public class Pong : Game
             ball.Update(gameTime);
         }
 
-        #region paddle movement
-
         KeyboardState kbState = Keyboard.GetState();
         if (kbState.IsKeyDown(Keys.W))
         {
-            _paddleDirection = new Vector2(0, -1);
+            _paddle.Direction = new Vector2(0, -1);
         }
         else if (kbState.IsKeyDown(Keys.S))
         {
-            _paddleDirection = new Vector2(0, 1);
+            _paddle.Direction = new Vector2(0, 1);
         }
         else
         {
-            _paddleDirection = new Vector2(0, 0);
-        }
-        _paddlePosition += _paddleDirection * _paddleSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-        if (_paddlePosition.Y <= _playAreaBoundingBox.Top)
-        {
-            _paddlePosition.Y = _playAreaBoundingBox.Top;
-        }
-        else if ((_paddlePosition.Y + _paddleDimensions.Y) >= _playAreaBoundingBox.Bottom)
-        {
-            _paddlePosition.Y = _playAreaBoundingBox.Bottom - _paddleDimensions.Y;
+            _paddle.Direction = new Vector2(0, 0);
         }
 
-        #endregion
+        _paddle.Update(gameTime);
+
         base.Update(gameTime);
     }
 
@@ -123,15 +109,7 @@ public class Pong : Game
         {
             ball.Draw(_spriteBatch);
         }
-
-        // draw paddle as a stretched 1x1 pixel
-        Rectangle paddleRect = new Rectangle(
-            (int)_paddlePosition.X,
-            (int)_paddlePosition.Y,
-            (int)_paddleDimensions.X,
-            (int)_paddleDimensions.Y
-        );
-        _spriteBatch.Draw(_pixel, paddleRect, Color.White);
+        _paddle.Draw(_spriteBatch);
 
         _spriteBatch.End();
 
